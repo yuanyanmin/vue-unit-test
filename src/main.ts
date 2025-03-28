1da616f06efb170386e0cf6c27532e6e5e655779
yuanyanmin <yuanyanmin@pachiratech.com>
2025-03-28 12:01:18 +0800

    删掉无用代码

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
-import '../mock/table.js'
 
 const app = createApp(App)
 
